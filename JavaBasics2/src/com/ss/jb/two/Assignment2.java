483bfbdc20a1793be04d2b069f563a6a7fbc9c8f
Sean Walkling <sean.walkling@smoothstack.com>
2021-07-13 11:52:54 -0700

    Corrected JB2 Assignment 2 to display position of max value

--- JavaBasics2/src/com/ss/jb/two/Assignment2.java
+++ JavaBasics2/src/com/ss/jb/two/Assignment2.java
@@ -14,17 +14,22 @@ public class Assignment2 {
 	 */
 	public static void main(String[] args) {
 		
-		int array2D[][] = {{3, 5, 9, 27, 51}, {28, 187, 78, 4, 0}, {66, 102, 4, 91, 28}};
+		int array2D[][] = {{3, 5, 9, 27, 515}, {28, 187, 78, 4, 0}, {66, 256, 4, 91, 31}};
 		int maxInt = 0;
+		int position[] = {0,0};
 
 		for(int i = 0; i < array2D.length; i++) {
 			for(int j = 0; j < array2D[i].length; j++) {
 				if(array2D[i][j] > maxInt) {
 					maxInt = array2D[i][j];
+					position[0] = i;
+					position[1] = j;
+					
 				}
 			}
 		}
-		System.out.print(maxInt);
+		System.out.print(maxInt + "\n");
+		System.out.print("Located at position: " + position[0] + ", " + position[1]);
 	}
 
 }
